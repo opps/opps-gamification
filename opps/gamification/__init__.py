42ea69accf2dc7d65c6045c214ec06315a602cb3
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-27 15:02:33 -0300

    create init gamification

---
+++ opps/gamification/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 1, 0)
+
+__version__ = ".".join(map(str, VERSION))
+__status__ = "Development"
+__description__ = u"Gamification App for Opps CMS"
+
+__author__ = u"Thiago Avelino"
+__credits__ = []
+__email__ = u"thiagoavelinoster@gmail.com"
+__license__ = u"MIT License"
+__copyright__ = u"Copyright 2013, Opps Projects"
