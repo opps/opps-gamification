4717de24d4b7bc55501a5c4ba70ffb63dbcb7e84
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-01 19:21:53 -0300

    Add action field on rules class

--- opps/gamification/models/rules.py
+++ opps/gamification/models/rules.py
@@ -11,6 +11,13 @@ class Rule(Publishable, Slugged):
     name = models.CharField(_(u"Name"), max_length=140, db_index=True)
     application = models.CharField(_(u'Application'), max_length=150,
                                    choices=OPPS_APPS)
+    action = models.CharField(_(u"Action"), max_length=140,
+                              choices=(
+                                  ('pre_save', _(u'Pre save')),
+                                  ('pos_save', _(u'Pos save')),
+                                  ('pre_delete', _(u'Pre delete')),
+                                  ('pos_delete', _(u'Pos delete'))),
+                              db_index=True)
     badge = models.ForeignKey('gamification.Badge')
     score = models.ForeignKey('gamification.Score')
 
