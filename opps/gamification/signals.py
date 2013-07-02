edb0185df790e66251d7a2ef064deb9cb5e851af
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-02 19:35:18 -0300

    Basic generic_signal

---
+++ opps/gamification/signals.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def generic_signal(sender, instance, created, **kwargs):
+    import pdb; pdb.set_trace()
+    """
+    obj, create = Redirect.objects.get_or_create(
+        old_path=instance.get_absolute_url(),
+        site=instance.site,
+        new_path=instance.url)
+    obj.save()
+    """
+    pass
